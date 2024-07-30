00cc37d0aaa141972196dc97ce3de4244da394e2
Joshua Klein <mobiusklein@gmail.com>
2024-07-30 18:35:10 -0400

    Specify  documentation dependencies

--- implementations/python/setup.py
+++ implementations/python/setup.py
@@ -3,15 +3,25 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mzpaf',
-    packages=find_packages(exclude=('tests',)),
+    name="mzpaf",
+    packages=find_packages(exclude=("tests",)),
     requires=["pyteomics"],
-    version='0.1.0-alpha',
-    description='HUPO-PSI Peptide peak annotation format',
+    extras_require=dict(
+        docs=[
+            "sphinx",
+            "sphinx-rtd-theme",
+            "numpydoc>=1,<2",
+            "sphinx_click",
+            "myst-parser",
+            "sphinx-autobuild",
+        ],
+    ),
+    version="0.1.0-alpha",
+    description="HUPO-PSI Peptide peak annotation format",
     classifiers=[
         "Intended Audience :: Science/Research",
         "Programming Language :: Python :: 3 :: Only",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
-        "Development Status :: 3 - Alpha"
+        "Development Status :: 3 - Alpha",
     ],
 )
